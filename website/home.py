6f60d2def21fe19faf7d3f7b33cba992b777e430
remote5izy <103296316+remote5izy@users.noreply.github.com>
2023-11-26 22:47:18 +0800

    z2

--- website/home.py
+++ website/home.py
@@ -6,6 +6,11 @@ home = Blueprint('home', __name__)
 @home.route('/')
 def index():
     return render_template("z.html")
+
+@home.route('/group')
+def index2():
+    return render_template("z2.html")
+
 @home.route('/about')
 def about():
     return 'About'
