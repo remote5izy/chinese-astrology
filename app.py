8310c3653af9d1ef3f048c9a5387f49d33b561b0
remote5izy <103296316+remote5izy@users.noreply.github.com>
2023-12-08 13:56:01 +0800

    0

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from fastapi import FastAPI, Request, APIRouter, Form, Depends
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from function import createForm
 
 app = FastAPI(debug=True)
 app.mount("/static/", StaticFiles(directory="static", html=True), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def temp(request: Request):
     return templates.TemplateResponse("temp.html", {"request": request})
 
+
+@app.get("/z", response_class=HTMLResponse)
+async def z(request: Request):
+    return templates.TemplateResponse("z.html", {"request": request})
+
+
 @app.post("/grace")
-async def grace(request: Request, year: int = Form(...), month: int = Form(...), day: int = Form(...), hour: int = Form(...)):
-    context = {"request": request, "year": year, "month": month, "day": day, "hour": hour}
-    print(context)
-    return  {"message": "success"}
+async def grace(
+    year: int = Form(...),
+    month: int = Form(...),
+    day: int = Form(...),
+    hour: int = Form(...),
+):
+    result = createForm(year, month, day, hour)
+    return {"result": result}
---
+++ function.py
@@ -0,0 +1,326 @@
+heavenlyStemList = ["癸", "甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬"]  # 癸 甲 乙 丙
+earthlyBranchList = [
+    "亥",
+    "子",
+    "丑",
+    "寅",
+    "卯",
+    "辰",
+    "巳",
+    "午",
+    "未",
+    "申",
+    "酉",
+    "戌",
+]  # 亥 子 丑 寅
+gong = [
+    "命宮",
+    "父母宮",
+    "福德宮",
+    "田宅宮",
+    "官祿宮",
+    "交友宮",
+    "遷移宮",
+    "疾厄宮",
+    "財帛宮",
+    "子女宮",
+    "夫妻宮",
+    "兄弟宮",
+]  # 命宮 父母 福德 田宅
+house = [[0 for _ in range(6)] for _ in range(12)]  # 12個 [天干, 地支, 宮, 身宮與否, 紫微星系, 天府星系]
+
+zw = ["紫微", 0, 0, 0, "廉貞", 0, 0, "天同", "武曲", "太陽", 0, "天機"]
+tf = ["天府", "太陰", "貪狼", "巨門", "天相", "天梁", "七殺", 0, 0, 0, "破軍", 0]
+
+element_mapping = {
+    ("甲", "子"): "海中金",
+    ("乙", "丑"): "海中金",
+    ("壬", "申"): "劍鋒金",
+    ("癸", "酉"): "劍鋒金",
+    ("庚", "辰"): "白蠟金",
+    ("辛", "巳"): "白蠟金",
+    ("甲", "午"): "砂中金",
+    ("乙", "未"): "砂中金",
+    ("壬", "寅"): "金箔金",
+    ("癸", "卯"): "金箔金",
+    ("庚", "戌"): "釵釧金",
+    ("辛", "亥"): "釵釧金",
+    ("壬", "午"): "楊柳木",
+    ("癸", "未"): "楊柳木",
+    ("戊", "辰"): "大林木",
+    ("己", "巳"): "大林木",
+    ("庚", "寅"): "松柏木",
+    ("辛", "卯"): "松柏木",
+    ("戊", "戌"): "平地木",
+    ("己", "亥"): "平地木",
+    ("壬", "子"): "桑拓木",
+    ("癸", "丑"): "桑拓木",
+    ("庚", "申"): "石榴木",
+    ("辛", "酉"): "石榴木",
+    ("丙", "子"): "澗下水",
+    ("丁", "丑"): "澗下水",
+    ("甲", "申"): "泉中水",
+    ("乙", "酉"): "泉中水",
+    ("壬", "辰"): "長流水",
+    ("癸", "巳"): "長流水",
+    ("丙", "午"): "天河水",
+    ("丁", "未"): "天河水",
+    ("甲", "寅"): "大溪水",
+    ("乙", "卯"): "大溪水",
+    ("壬", "戌"): "大海水",
+    ("癸", "亥"): "大海水",
+    ("丙", "寅"): "爐中火",
+    ("丁", "卯"): "爐中火",
+    ("甲", "戌"): "山頭火",
+    ("乙", "亥"): "山頭火",
+    ("戊", "子"): "霹靂火",
+    ("己", "丑"): "霹靂火",
+    ("丙", "申"): "山下火",
+    ("丁", "酉"): "山下火",
+    ("甲", "辰"): "覆燈火",
+    ("乙", "巳"): "覆燈火",
+    ("戊", "午"): "天上火",
+    ("己", "未"): "天上火",
+    ("庚", "午"): "路傍土",
+    ("辛", "未"): "路傍土",
+    ("戊", "寅"): "城頭土",
+    ("己", "卯"): "城頭土",
+    ("丙", "戌"): "屋上土",
+    ("丁", "亥"): "屋上土",
+    ("庚", "子"): "壁上土",
+    ("辛", "丑"): "壁上土",
+    ("戊", "申"): "大驛土",
+    ("己", "酉"): "大驛土",
+    ("丙", "辰"): "砂中土",
+    ("丁", "巳"): "砂中土",
+}
+
+
+def get_element(x, y):
+    return element_mapping.get((x, y), None)
+
+
+def water(day):
+    if day == 22 or day == 23:
+        x = 1
+    if day == 1 or day == 24 or day == 25:
+        x = 2
+    if day == 2 or day == 3 or day == 26 or day == 27:
+        x = 3
+    if day == 4 or day == 5 or day == 28 or day == 29:
+        x = 4
+    if day == 6 or day == 7 or day == 30:
+        x = 5
+    if day == 8 or day == 9:
+        x = 6
+    if day == 10 or day == 11:
+        x = 7
+    if day == 12 or day == 13:
+        x = 8
+    if day == 14 or day == 15:
+        x = 9
+    if day == 16 or day == 17:
+        x = 10
+    if day == 18 or day == 19:
+        x = 11
+    if day == 20 or day == 21:
+        x = 0
+    y = (18 - x) % 12
+    return [x, y]
+
+
+def wood(day):
+    if day == 25:
+        x = 1
+
+    if day == 2 or day == 28:
+        x = 2
+
+    if day == 3 or day == 5:
+        x = 3
+
+    if day == 6 or day == 8:
+        x = 4
+
+    if day == 1 or day == 9 or day == 11:
+        x = 5
+
+    if day == 4 or day == 12 or day == 14:
+        x = 6
+
+    if day == 7 or day == 15 or day == 17:
+        x = 7
+
+    if day == 10 or day == 18 or day == 20:
+        x = 8
+
+    if day == 13 or day == 21 or day == 23:
+        x = 9
+
+    if day == 16 or day == 24 or day == 26:
+        x = 10
+
+    if day == 19 or day == 27 or day == 29:
+        x = 11
+
+    if day == 22 or day == 30:
+        x = 0
+    y = (18 - x) % 12
+    return [x, y]
+
+
+def gold(day):
+    if day == 5:
+        x = 1
+
+    if day == 3 or day == 9:
+        x = 2
+
+    if day == 4 or day == 7 or day == 13:
+        x = 3
+
+    if day == 8 or day == 11 or day == 17:
+        x = 4
+
+    if day == 2 or day == 12 or day == 15 or day == 21:
+        x = 5
+
+    if day == 6 or day == 16 or day == 19 or day == 25:
+        x = 6
+
+    if day == 10 or day == 20 or day == 23 or day == 29:
+        x = 7
+
+    if day == 14 or day == 24 or day == 27:
+        x = 8
+
+    if day == 18 or day == 28:
+        x = 9
+
+    if day == 22:
+        x = 10
+
+    if day == 26:
+        x = 11
+
+    if day == 1 or day == 30:
+        x = 0
+    y = (18 - x) % 12
+    return [x, y]
+
+
+def mud(day):
+    if day == 7:
+        x = 1
+
+    if day == 4 or day == 12:
+        x = 2
+
+    if day == 5 or day == 9 or day == 17:
+        x = 3
+
+    if day == 10 or day == 14 or day == 22:
+        x = 4
+
+    if day == 3 or day == 15 or day == 19 or day == 27:
+        x = 5
+
+    if day == 8 or day == 20 or day == 24:
+        x = 6
+
+    if day == 1 or day == 13 or day == 25 or day == 29:
+        x = 7
+
+    if day == 6 or day == 18 or day == 30:
+        x = 8
+
+    if day == 11 or day == 13:
+        x = 9
+
+    if day == 16 or day == 28:
+        x = 10
+
+    if day == 21:
+        x = 11
+
+    if day == 26 or day == 2:
+        x = 0
+    y = (18 - x) % 12
+    return [x, y]
+
+
+def fire(day):
+    if day == 9 or day == 19:
+        x = 1
+
+    if day == 5 or day == 15 or day == 25:
+        x = 2
+
+    if day == 6 or day == 11 or day == 21:
+        x = 3
+
+    if day == 12 or day == 17 or day == 27:
+        x = 4
+
+    if day == 4 or day == 18 or day == 23:
+        x = 5
+
+    if day == 10 or day == 24 or day == 29:
+        x = 6
+
+    if day == 2 or day == 16 or day == 30:
+        x = 7
+
+    if day == 8 or day == 22:
+        x = 8
+
+    if day == 14 or day == 28:
+        x = 9
+
+    if day == 1 or day == 20:
+        x = 10
+
+    if day == 7 or day == 26:
+        x = 11
+
+    if day == 3 or day == 13:
+        x = 0
+    y = (18 - x) % 12
+    return [x, y]
+
+
+def createForm(year, month, day, hour):
+    house[(1 + month + hour) % 12][3] = "身宮"
+    temp = 3 + month - hour
+    for i in range(12):
+        house[(temp + i) % 12][2] = gong[i]
+        house[(3 + i) % 12][0] = heavenlyStemList[
+            (((year - 1803) % 5) * 2 + 1 + i) % 10
+        ]
+        house[i][1] = earthlyBranchList[i]
+    ny = get_element(house[(temp) % 12][0], house[(temp) % 12][1])
+    if ny[-1] == "水":
+        for i in range(12):
+            house[(water(day)[0] + i) % 12][4] = zw[i]
+            house[(water(day)[1] + i) % 12][5] = tf[i]
+    elif ny[-1] == "木":
+        for i in range(12):
+            house[(wood(day)[0] + i) % 12][4] = zw[i]
+            house[(wood(day)[1] + i) % 12][5] = tf[i]
+    elif ny[-1] == "金":
+        for i in range(12):
+            house[(gold(day)[0] + i) % 12][4] = zw[i]
+            house[(gold(day)[1] + i) % 12][5] = tf[i]
+    elif ny[-1] == "土":
+        for i in range(12):
+            house[(mud(day)[0] + i) % 12][4] = zw[i]
+            house[(mud(day)[1] + i) % 12][5] = tf[i]
+    elif ny[-1] == "火":
+        for i in range(12):
+            house[(fire(day)[0] + i) % 12][4] = zw[i]
+            house[(fire(day)[1] + i) % 12][5] = tf[i]
+
+    return house
+
+
+
