e5b9669e51fb154cac2b45cfdd8c80a678de1e38
remote5izy <103296316+remote5izy@users.noreply.github.com>
2023-12-08 14:06:47 +0800

    chat

--- static/temp.js
+++ static/temp.js
@@ -1,19 +1,23 @@
-const myform = document.getElementById("calculatorForm");
+document.addEventListener("DOMContentLoaded", function () {
+    const myform = document.getElementById("calculatorForm");
 
-myform.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(myform);
+    myform.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const formData = new FormData(myform);
 
-
-    const response =
-        await fetch("/grace", {
+        const response = await fetch("/grace", {
             method: "POST",
             body: formData
         });
 
-    const req = await response.json();
+        const req = await response.json();
 
-    console.log(req);
-    const ans = document.getElementById("ans");
-    console.log(ans);
+        console.log(req);
+
+        const ans = document.getElementById("ans");
+        console.log(ans); // This should now log the "ans" element
+
+        // If you want to log the content of the "ans" element:
+        console.log(ans.innerText);
+    });
 });
