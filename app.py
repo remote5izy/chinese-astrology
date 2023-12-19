29e4cedf0b3ab382e8da4fb9940dcfa907dccc20
remote5izy <103296316+remote5izy@users.noreply.github.com>
2023-12-19 23:56:54 +0800

    0

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 from starlette.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from function import createForm
+from fastapi.middleware.cors import CORSMiddleware
+
+class Data(BaseModel):
+    year: int
+    month: int
+    day: int
+    hour: int
 
 app = FastAPI(debug=True)
 app.mount("/static/", StaticFiles(directory="static", html=True), name="static")
 templates = Jinja2Templates(directory="templates")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", response_class=HTMLResponse)
 async def temp(request: Request):
@@ -27,4 +42,9 @@ async def grace(
     hour: int = Form(...),
 ):
     result = createForm(year, month, day, hour)
+    return result
+
+@app.post("/cloud")
+async def grace( item:Data):
+    result = createForm(item.year, item.month, item.day, item.hour)
     return result
